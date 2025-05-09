3e95f2acce59e0a93b72e1fb24854cb29437d533
NandiniRaja030 <nandini.rajaradi@gmail.com>
2025-05-09 18:59:40 +0530

    commit

---
+++ world-of-dune/script.js
@@ -0,0 +1,17 @@
+//Download Alert
+function down() {
+    window.alert("Your file will download.");
+}
+
+//Theme Change Toggle
+function themeChange() {
+    var thch = document.body;
+    thch.classList.toggle("themeChange");
+}
+
+//Console Log
+window.onload = function() {
+    console.log("Welcome to The World of DUNE!");
+};
+
+window.alert("I am an avid Dune fanatic, so a simple side project I did during my boredom. These are real links to actual books. (The 3rd book is my favourite) Also I prefer the dark theme ;)")
